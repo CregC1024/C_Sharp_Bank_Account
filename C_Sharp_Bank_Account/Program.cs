a43144fa09747b25b4984831fc9514f1cafadc9a
Creg Sullivan <cregcsullivan1024@gmail.com>
2022-01-22 02:31:39 -0600

    push it!

--- C_Sharp_Bank_Account/Program.cs
+++ C_Sharp_Bank_Account/Program.cs
@@ -7,12 +7,13 @@ namespace classes
         static void Main(string[] args)
         {
             var account = new BankAccount("<Creg>", 1000);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            Console.WriteLine($"Account # {account.Number}  was created for {account.Owner} with ${account.Balance} dollars initial balance.");
         }
     }
 
     public class BankAccount
     {
+        private static int accountNumberSeed = 1234567890;
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
@@ -21,6 +22,9 @@ namespace classes
         {
             this.Owner = name;
             this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
